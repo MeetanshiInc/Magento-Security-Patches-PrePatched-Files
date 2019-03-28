e03d09624285099671a2ba36ce7f6dedc62e9e62
Shivbhadrasinh Gohil <shivbhadrasinh@meetanshi.com>
2019-03-28 18:59:04 +0530

    SUPEE 11086

---
+++ SUPEE-11086/CE-1.9.3.10/app/code/core/Mage/Adminhtml/Block/System/Store/Edit.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Adminhtml
+ * @copyright  Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Adminhtml store edit
+ *
+ * @category   Mage
+ * @package    Mage_Adminhtml
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+
+class Mage_Adminhtml_Block_System_Store_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
+{
+    /**
+     * Init class
+     *
+     */
+    public function __construct()
+    {
+        $backupAvailable =
+            Mage::getSingleton('admin/session')->isAllowed('system/tools/backup')
+            && Mage::helper('core')->isModuleEnabled('Mage_Backup')
+            && !Mage::getStoreConfigFlag('advanced/modules_disable_output/Mage_Backup');
+        switch (Mage::registry('store_type')) {
+            case 'website':
+                $this->_objectId = 'website_id';
+                $saveLabel   = Mage::helper('core')->__('Save Website');
+                $deleteLabel = Mage::helper('core')->__('Delete Website');
+                $deleteUrl   = $this->_getDeleteUrl(Mage::registry('store_type'), $backupAvailable);
+                break;
+            case 'group':
+                $this->_objectId = 'group_id';
+                $saveLabel   = Mage::helper('core')->__('Save Store');
+                $deleteLabel = Mage::helper('core')->__('Delete Store');
+                $deleteUrl   = $this->_getDeleteUrl(Mage::registry('store_type'), $backupAvailable);
+                break;
+            case 'store':
+                $this->_objectId = 'store_id';
+                $saveLabel   = Mage::helper('core')->__('Save Store View');
+                $deleteLabel = Mage::helper('core')->__('Delete Store View');
+                $deleteUrl   = $this->_getDeleteUrl(Mage::registry('store_type'), $backupAvailable);
+                break;
+        }
+        $this->_controller = 'system_store';
+
+        parent::__construct();
+
+        $this->_updateButton('save', 'label', $saveLabel);
+        $this->_updateButton('delete', 'label', $deleteLabel);
+        $this->_updateButton('delete', 'onclick', 'setLocation(\''.$deleteUrl.'\');');
+
+        if (!Mage::registry('store_data')->isCanDelete()) {
+            $this->_removeButton('delete');
+        }
+        if (Mage::registry('store_data')->isReadOnly()) {
+            $this->_removeButton('save')->_removeButton('reset');
+        }
+    }
+
+    /**
+     * Get Header text
+     *
+     * @return string
+     */
+    public function getHeaderText()
+    {
+        switch (Mage::registry('store_type')) {
+            case 'website':
+                $editLabel = Mage::helper('core')->__('Edit Website');
+                $addLabel  = Mage::helper('core')->__('New Website');
+                break;
+            case 'group':
+                $editLabel = Mage::helper('core')->__('Edit Store');
+                $addLabel  = Mage::helper('core')->__('New Store');
+                break;
+            case 'store':
+                $editLabel = Mage::helper('core')->__('Edit Store View');
+                $addLabel  = Mage::helper('core')->__('New Store View');
+                break;
+        }
+
+        return Mage::registry('store_action') == 'add' ? $addLabel : $editLabel;
+    }
+
+    /**
+     * Create URL depending on backups
+     *
+     * @param string $storeType
+     * @param bool $backupAvailable
+     * @return string
+     */
+    public function _getDeleteUrl($storeType, $backupAvailable = false)
+    {
+        $storeType = uc_words($storeType);
+        if ($backupAvailable) {
+            $deleteUrl   = $this->getUrl('*/*/delete' . $storeType, array('item_id' => Mage::registry('store_data')->getId()));
+        } else {
+            $deleteUrl   = $this->getUrl(
+                '*/*/delete' . $storeType . 'Post',
+                array(
+                    'item_id' => Mage::registry('store_data')->getId(),
+                    'form_key' => Mage::getSingleton('core/session')->getFormKey()
+                )
+            );
+        }
+
+        return $deleteUrl;
+    }
+}
